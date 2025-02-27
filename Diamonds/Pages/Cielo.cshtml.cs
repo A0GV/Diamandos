227086b008123596e60601130ca696c3fe5855af
Cielo Maria Vega Godoy <a01741542@tec.mx>
2025-02-27 12:54:46 -0600

    Add Cielo page with model and styling for image display

---
+++ Diamonds/Pages/Cielo.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diamonds.Pages;
+
+public class CieloModel : PageModel
+{
+
+    public void OnGet()
+    {
+        
+    }
+}
