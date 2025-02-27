7f47b175d1a0a40b25778c5e6447338007744686
Regina <a00840840@tec.mx>
2025-02-27 13:09:03 -0600

    holaaaa

---
+++ Diamonds/Pages/regina.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Diamonds.Pages;
+
+public class ReginaModel : PageModel
+{
+
+    public void OnGet()
+    {
+
+    }
+}
